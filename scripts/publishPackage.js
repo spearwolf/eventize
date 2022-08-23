54886f5b468450a4a71cb8f2bb3c3b2482029aa0
Wolfger Schramm <wolfger@spearwolf.de>
2022-08-23 13:23:40 +0200

    add github build and deploy workflow

---
+++ scripts/publishPackage.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+const {exec} = require('child_process');
+const process = require('process');
+
+const pkgJson = require('../package.json');
+
+function publishPackage() {
+  exec(`npm publish --access public --dry-run`, (error, stdout, stderr) => {
+    console.error(stderr);
+    console.log(stdout);
+
+    if (!error) {
+      process.exit(0);
+    } else {
+      process.exit(error);
+    }
+  });
+}
+
+exec(`npm show ${pkgJson.name} versions --json`, (error, stdout, stderr) => {
+  if (!error) {
+    const versions = JSON.parse(stdout);
+    if (versions.includes(pkgJson.version)) {
+      console.log(
+        'skipping publish, version',
+        pkgJson.version,
+        'already published',
+      );
+      process.exit(0);
+    } else {
+      publishPackage();
+    }
+  } else {
+    console.error(`exec error: ${stderr}`);
+  }
+});
