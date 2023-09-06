0d15e8ab5b63df3f43681c08b8b497b5a45cb8f8
Wolfger Schramm <wolfger@spearwolf.de>
2023-09-06 10:40:38 +0200

    bundle with banner

--- scripts/prependBanner/makeBanner.mjs
+++ scripts/prependBanner/makeBanner.mjs
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-export default ({name, repository, version}) => ({
-  banner: `/*!
+export default ({name, repository, version}) =>
+  `/*!
 =============================================================================
 ${name} ${version}
 — ${repository.url}
@@ -19,5 +19,4 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-*/`,
-});
+*/`;
--- tsup.config.js
+++ tsup.config.js
@@ -3,6 +3,7 @@ import {defineConfig} from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
+  outDir: 'build',
   splitting: false,
   sourcemap: false,
   minify: true,
